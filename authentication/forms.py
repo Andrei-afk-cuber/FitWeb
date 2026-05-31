28398c62251cff2a54b3f4e360ec67981dbf3049
Andrei Lyakh <Edgar.digl@mail.ru>
2026-05-31 18:32:01 +0300

    Added front for authentication app.

--- authentication/forms.py
+++ authentication/forms.py
@@ -22,21 +22,25 @@ class UserForm(forms.ModelForm):
         ]
 
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+        label="Пароль",
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Пароль от 8-ми символов"}),
         validators=[MinLengthValidator(8)],
     )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # add CSS class for every widget
+        # add CSS class for every widget and placeholders if necessary
         self.fields["gender"].widget.attrs["class"] = "form-select"
         self.fields["activity_status"].widget.attrs["class"] = "form-select"
         self.fields["target"].widget.attrs["class"] = "form-select"
 
         self.fields["email"].widget.attrs["class"] = "form-control"
+        self.fields["email"].widget.attrs["placeholder"] = "example@email.ru"
         self.fields["first_name"].widget.attrs["class"] = "form-control"
+        self.fields["first_name"].widget.attrs["placeholder"] = "Иван"
         self.fields["last_name"].widget.attrs["class"] = "form-control"
+        self.fields["last_name"].widget.attrs["placeholder"] = "Иванов"
         self.fields["weight"].widget.attrs["class"] = "form-control"
         self.fields["height"].widget.attrs["class"] = "form-control"
         self.fields["age"].widget.attrs["class"] = "form-control"
