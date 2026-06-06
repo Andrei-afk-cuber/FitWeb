b56fdd4a823e873d2122864aff218a0ac3218411
Andrei Lyakh <Edgar.digl@mail.ru>
2026-06-06 23:43:45 +0300

    Created beautiful HTML template and .css for calculator page.

--- calculator/views.py
+++ calculator/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.views import View
 
 from .utils import NutritionCalculator
+from authentication.models import User
 
 
 # main calculator view
 class CalculatorView(View):
     def get(self, request):
-        user = request.user
-        data = {}
-        # calculations
-        data['bmr'] = NutritionCalculator.calculate_bmr(user.gender, user.weight, user.height, user.age)
-        data['tdee'] = NutritionCalculator.calculate_tdee(data['bmr'], user.activity_status)
-        data['calories'] = NutritionCalculator.calculate_daily_calories(data['tdee'], user.target)
-        data = data | NutritionCalculator.calculate_macros(data['calories'], user.target, user.gender, user.weight)
 
-        return render(request, "calculator/main.html", data)
+        try:
+            user = request.user
+            data = {}
+            # calculations
+            data['bmr'] = NutritionCalculator.calculate_bmr(user.gender, user.weight, user.height, user.age)
+            data['tdee'] = NutritionCalculator.calculate_tdee(data['bmr'], user.activity_status)
+            data['calories'] = NutritionCalculator.calculate_daily_calories(data['tdee'], user.target)
+            data = data | NutritionCalculator.calculate_macros(data['calories'], user.target, user.gender, user.weight)
+
+            return render(request, "calculator/main.html", data)
+
+        except AttributeError:
+            return render(request, "calculator/main.html")
